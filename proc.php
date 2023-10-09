77c04d6a4bf8c65092f6000e4a6093f60f3a5054
bbulgacs <bruno.bulgacs@marupe.edu.lv>
2023-10-09 15:19:09 +0300

    connect5

--- proc.php
+++ proc.php
@@ -4,6 +4,10 @@ $p = $_POST["Password"];
 
 include('cfg,ph');
 $mysqli = new mysqli("SELECT * FROM users WHERE login=$l AND password$p")
+if(mysqli_num_rows($result) > 0){
+    echo "GOOOD!!!";
 
-echo " Hello - $p  ,Password - $p ";
+}
+
+echo " Hello - $l  ,Password - $p ";
 ?>
