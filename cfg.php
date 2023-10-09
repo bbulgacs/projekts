903f20ac5c59bd06a440a767a3c87fae948d4882
bbulgacs <bruno.bulgacs@marupe.edu.lv>
2023-10-09 15:12:49 +0300

    connect3

--- cfg.php
+++ cfg.php
@@ -7,12 +7,6 @@ $db = "projekts1";
 
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $db);
+$mysqli = new mysqli($servername, $username, $password, $db);
 
-// Check connection
-if ($mysqli -> connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-    exit();
-  }
-  ?>
 
--- proc.php
+++ proc.php
@@ -1,5 +1,9 @@
 <?php
 $l = $_POST["login"];
 $p = $_POST["Password"];
+
+include('cfg,ph');
+$mysqli = new mysqli("SELECT * FROM users WHERE login=$l and password")
+
 echo " Hello - $p  ,Password - $p ";
 ?>
