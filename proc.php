ada7e3203a1a48eefff29630b65cf680082bdff6
bbulgacs <bruno.bulgacs@marupe.edu.lv>
2023-10-09 15:14:08 +0300

    connect4

--- proc.php
+++ proc.php
@@ -3,7 +3,7 @@ $l = $_POST["login"];
 $p = $_POST["Password"];
 
 include('cfg,ph');
-$mysqli = new mysqli("SELECT * FROM users WHERE login=$l and password")
+$mysqli = new mysqli("SELECT * FROM users WHERE login=$l AND password$p")
 
 echo " Hello - $p  ,Password - $p ";
 ?>
