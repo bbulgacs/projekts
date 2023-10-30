f2717255020e7ddbe5a989d239c4d2327974f5ce
bbulgacs <144686286+bbulgacs@users.noreply.github.com>
2023-10-30 15:22:00 +0200

    JS

---
+++ main.js
@@ -0,0 +1,12 @@
+function enableButton(){
+    let a = document.getElementById('loginName').value
+    let b = document.getElementById('loginPassword').value
+    alert(a +'\n' + b)
+    if ( a.length > 0 && b.length > 0){
+        document.getElementById('login').disabled = false 
+
+}
+else{
+    document.getElementById('login').disabled = true
+        }
+}   
